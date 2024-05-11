a0a90d034abebbe5435152711b0e5a068358220c
Ayaan Eusufzai <97022894+2048hertz@users.noreply.github.com>
2024-05-12 01:13:18 +0600

    rum fix

--- phase2.sh
+++ phase2.sh
@@ -13,11 +13,8 @@ cd
 sudo sh /usr/bin/RobertOS-assets/robertdesetup.sh
 sudo apt-get remove -y rp-prefapps lxappearance lxde lxde-* lxinput lxmenu-data lxpanel lxpolkit lxrandr lxsession* lxsession lxshortcut lxtask lxterminal
 sudo apt-get autoremove -y && sudo apt-get autoclean -y
-sudo wget https://github.com/2048hertz/robertosupdatemanager/releases/download/1.0/rum.zip
-sudo unzip rum.zip
-cd /rum/
+sudo wget https://github.com/2048hertz/robertosupdatemanager/releases/download/1.0/rum.sh
 sudo sh rum.sh
-cd
 
 
 # MAIN CONFIG FINISHED
