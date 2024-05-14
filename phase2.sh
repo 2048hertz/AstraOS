39c109548175c09b97cb13981fc39a24af9db6c8
Ayaan Eusufzai <97022894+2048hertz@users.noreply.github.com>
2024-05-14 23:08:32 +0600

    pi apps addition

--- phase2.sh
+++ phase2.sh
@@ -13,6 +13,7 @@ cd
 sudo sh /usr/bin/RobertOS-assets/robertdesetup.sh
 sudo apt-get remove -y rp-prefapps lxappearance lxde lxde-* lxinput lxmenu-data lxpanel lxpolkit lxrandr lxsession* lxsession lxshortcut lxtask lxterminal
 sudo apt-get autoremove -y && sudo apt-get autoclean -y
+wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash
 sudo wget https://github.com/2048hertz/robertosupdatemanager/releases/download/1.0/rum.sh
 sudo sh rum.sh
 
