19696a7085f645b15498f44244caeed673091069
Ayaan Eusufzai <97022894+2048hertz@users.noreply.github.com>
2024-05-26 01:21:42 +0600

    sed fix

--- themesetup.sh
+++ themesetup.sh
@@ -5,21 +5,20 @@
 git clone https://github.com/vinceliuice/Orchis-theme.git
 
 # Install necessary dependencies for building the theme
-sudo apt-get install -y gtk2-engines-murrine gtk2-engines-pixbuf sassc
+sudo apt-get install -y gtk2-engines-murrine gtk2-engines-pixbuf sassc software-properties-common
+
+# Add the Numix PPA and install the Numix Square icon theme
+sudo apt-get install -y dirmngr
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1DCBDC01F9BA0ADA
+echo "deb http://ppa.launchpad.net/numix/ppa/ubuntu bionic main" | sudo tee /etc/apt/sources.list.d/numix-ppa.list
+sudo apt-get update
+sudo apt-get install -y numix-icon-theme numix-icon-theme-square
 
 # Navigate to the cloned directory
 cd Orchis-theme
 
 # Run the installation script
 ./install.sh --tweaks solid
-
-# Install the Numix Square icon theme manually
-mkdir -p ~/.icons
-cd ~/.icons
-wget -O Numix-Square.tar.gz https://github.com/numixproject/numix-icon-theme-square/archive/master.tar.gz
-tar -xzvf Numix-Square.tar.gz
-mv numix-icon-theme-square-master Numix-Square
-rm Numix-Square.tar.gz
 
 # Apply the Orchis theme and Numix Square icon theme using sed
 xfce_config_file="$HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml"
@@ -34,6 +33,4 @@ else
     echo "XFCE config file not found. Please ensure XFCE is installed and configured properly."
 fi
 
-# Reload the XFCE environment to apply the changes
-xfwm4 --replace &
-xfce4-panel -r &
+echo "Please reboot the system to apply all changes"
