6cb8e18083c9af6e106224922ae8394b78b217dd
Ayaan Eusufzai <97022894+2048hertz@users.noreply.github.com>
2024-05-27 17:36:22 +0600

    RobertOS > AstraOS rebrand

--- phase2.sh
+++ phase2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# Welcome to the Robert Computer Operating System build script 2
+# Welcome to the Astra Computer Operating System build script 2
 # Most of the code that is important to phase 2 is in the desetup.sh script, for more information please contact 2048megahertz@proton.me
 # Written by Ayaan Eusufzai
 # Version Teddy-Bear V1
 
 # Add camera installation code here
 
-sudo sh /usr/bin/RobertOS-assets/desetup.sh
+sudo sh /usr/bin/AstraOS-assets/desetup.sh
 sudo apt-get remove -y rp-prefapps lxappearance lxde lxde-* lxinput lxmenu-data lxpanel lxpolkit lxrandr lxsession* lxsession lxshortcut lxtask lxterminal
 sudo apt-get autoremove -y && sudo apt-get autoclean -y
 sudo wget https://github.com/2048hertz/robertosupdatemanager/releases/download/1.0/rum.sh
@@ -16,4 +16,9 @@ sudo sh rum.sh
 # MAIN CONFIG FINISHED
 
 # Finishing up
+<<<<<<< HEAD
+sudo sh /usr/bin/AstraOS-assets/prepare_image.sh
+
+=======
 sudo bash prepare_image.sh --create-iso
+>>>>>>> 501fd5a7f027dbf043a612373135e90c9af707dc
