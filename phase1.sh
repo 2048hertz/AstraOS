43a65a85f2629e736c42a09fe35d28ddb1c74734
Ayaan Eusufzai <97022894+2048hertz@users.noreply.github.com>
2024-05-27 17:47:26 +0600

    phase 1 structural update + implementation of gdm

--- phase1.sh
+++ phase1.sh
@@ -6,38 +6,47 @@
 # Please make sure you have the correct version of Raspberry Pi OS which this script is made for, the latest version of the base operating system will be installed automatically from the script
 # Started: 7/5/24 1:28 AM Bangladesh Time
 
+sudo apt update -y && sudo apt upgrade -y
+
+# Install XFCE4 and related packages
 sudo apt install -y xfce4 xfce4-terminal xfce4-goodies firefox
 
-# Light DM Configuration Update
+# Install GDM and remove LightDM
+sudo apt install -y gdm3
+sudo apt remove -y lightdm
 
-cd /etc/lightdm
-sudo cp /etc/lightdm/lightdm.conf /etc/lightdm/lightdm.conf.bak 
-sed -i 's/^user-session=.*/user-session=xfce4-session/' /etc/lightdm/lightdm.conf
-sed -i 's/^greeter-session=.*/greeter-session=lightdm-gtk-greeter/' /etc/lightdm/lightdm.conf
-sed -i 's/^autologin-session=.*/autologin-session=xfce4-session/' /etc/lightdm/lightdm.conf
-cd
+# Set GDM as the default display manager
+sudo dpkg-reconfigure gdm3
 
-# Main 
+# Ensure XFCE4 is the default session
+echo "[daemon]
+DefaultSession=xfce.desktop
+" | sudo tee /etc/gdm3/custom.conf
+
+# Main
 
 sudo apt update -y && sudo apt upgrade -y
 sudo apt full-upgrade -y
-sudo apt remove chromium -y
+sudo apt remove -y chromium
 sudo apt remove -y desktop-base
 sudo apt remove -y eom
+
 # Addition of pi-apps
 git clone https://github.com/Botspot/pi-apps && ~/pi-apps/install
+
+# Install additional packages
 sudo apt install -y flatpak
 flatpak --user remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-sudo apt install -y snap
-sudo snap install -y core
+sudo apt install -y snapd
+sudo snap install core
 sudo apt install -y git build-essential cmake
 sudo wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list
 wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg
-sudo apt update && sudo apt install box64 -y
+sudo apt update && sudo apt install -y box64
 sudo sh /usr/bin/AstraOS-assets/rpi-overclock.sh
-sudo git clone https://github.com/AdnanHodzic/auto-cpufreq.git
-sudo cd auto-cpufreq && sudo ./auto-cpufreq-installer
-sudo apt-get install -y python3-gi
+git clone https://github.com/AdnanHodzic/auto-cpufreq.git
+cd auto-cpufreq && sudo ./auto-cpufreq-installer
+sudo apt install -y python3-gi
 sudo apt install -y network-manager-gnome
 sudo apt install -y aisleriot
 sudo apt install -y gnome-chess
